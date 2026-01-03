f7f8725c494296b7deaab0a52bccb769415dc750
jpricardo <joaopds.ricardo@gmail.com>
2026-01-03 20:26:12 -0300

    fix: width overflow

--- logwolf-server/frontend/app/components/ui/json-block.tsx
+++ logwolf-server/frontend/app/components/ui/json-block.tsx
@@ -8,7 +8,7 @@ export type Props = Omit<React.ComponentProps<typeof Card>, 'children'> & {
 export function JSONBlock({ data, className, ...props }: Props) {
 	return (
 		<Card className={cn('shadow-none overflow-x-auto', className)} {...props}>
-			<CardContent>
+			<CardContent className='max-w-0'>
 				<pre className='font-mono'>
 					<code>{JSON.stringify(data, undefined, '\t')}</code>
 				</pre>
